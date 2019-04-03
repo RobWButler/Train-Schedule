81875b168b45c870627aef3339a95146dc3eef6e
Robert Butler <robwbutler@gmail.com>
2019-04-03 17:31:46 -0400

    initial creation

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBcPe7CFxyh1lzGfKQiZ8ilY79x14qXEcQ",
+    authDomain: "gtsav2019.firebaseapp.com",
+    databaseURL: "https://gtsav2019.firebaseio.com",
+    projectId: "gtsav2019",
+    storageBucket: "gtsav2019.appspot.com",
+    messagingSenderId: "1022037644586"
+  };
+  firebase.initializeApp(config);
